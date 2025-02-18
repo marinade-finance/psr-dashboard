376660df11f2b074d91ae42693c431ce8fffcc27
Alexandru Stefan <alex.steefan@gmail.com>
2025-02-18 12:36:38 +0200

    feat: add more summary metrics on top

---
+++ src/components/complex-metric/complex-metric.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from './complex-metric.module.css'
+
+type Props = {
+    label: string
+    value: React.ReactNode
+    tooltipHtml?: string
+    'data-tooltip-id'?: string
+    'data-tooltip-html'?: string
+}
+
+export const ComplexMetric: React.FC<Props> = ({ label, value, ...tooltipsProps }) => {
+    return <div className={styles.complexMetricWrap} {...tooltipsProps}>
+        <div>{label}</div>
+        {value}
+    </div>
+};
