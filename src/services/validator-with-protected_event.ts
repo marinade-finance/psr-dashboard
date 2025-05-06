e27b5083db35e192506fb19dba9553f4909ffaf9
kronael <ondrej@vostal.net>
2025-05-06 20:07:51 +0200

    20250505 bid penalty [GEN-4854] (#25)
    * Add BidTooLowPenalty into ProtectedEvent settlements
    
    * Include the latest epoch penalties estimates
    
    * Include the latest epoch penalties estimates
    
    * Fix missaligned package

--- src/services/validator-with-protected_event.ts
+++ src/services/validator-with-protected_event.ts
@@ -1,6 +1,7 @@
 import { ProtectedEvent, fetchProtectedEvents } from "./protected-events";
 import { calculateProtectedEventEstimates } from "./protected-events-estimator";
 import { Validator, fetchValidators, fetchValidatorsWithEpochs } from "./validators";
+import { loadSam } from "./sam";
 
 export enum ProtectedEventStatus {
     DRYRUN, ESTIMATE, FACT
@@ -14,7 +15,7 @@ export type ProtectedEventWithValidator = {
 const LAST_DRYRUN_EPOCH = 608
 
 export const fetchProtectedEventsWithValidator = async (): Promise<ProtectedEventWithValidator[]> => {
-    const [{ validators }, { protected_events }] = await Promise.all([fetchValidatorsWithEpochs(3), fetchProtectedEvents()])
+  const [{ validators }, { protected_events }, { auctionResult }] = await Promise.all([fetchValidatorsWithEpochs(3), fetchProtectedEvents(), loadSam()])
 
     const estimatedProtectedEvents = await calculateProtectedEventEstimates(validators)
 
@@ -24,6 +25,7 @@ export const fetchProtectedEventsWithValidator = async (): Promise<ProtectedEven
     }
 
     let latestProcessedEpoch = 0
+    let latestEpoch = 0
     const protectedEventsWithValidator: ProtectedEventWithValidator[] = []
     for (const protectedEvent of protected_events) {
         latestProcessedEpoch = Math.max(protectedEvent.epoch, latestProcessedEpoch)
@@ -32,9 +34,29 @@ export const fetchProtectedEventsWithValidator = async (): Promise<ProtectedEven
     }
 
     for (const protectedEvent of estimatedProtectedEvents) {
+        latestEpoch = Math.max(protectedEvent.epoch, latestEpoch)
         if (protectedEvent.epoch > latestProcessedEpoch) {
             protectedEventsWithValidator.push({ status: ProtectedEventStatus.ESTIMATE, protectedEvent, validator: validatorsMap[protectedEvent.vote_account] ?? null })
         }
+    }
+
+    for (const entry of auctionResult.auctionData.validators) {
+      const validator = validatorsMap[entry.voteAccount] ?? null
+      const penalty = (parseFloat(validator?.activated_stake ?? "0")) * entry.revShare.bidTooLowPenaltyPmpe / 1000
+      if (penalty > 0) {
+        const protectedEvent = {
+          epoch: latestEpoch,
+          amount: penalty,
+          vote_account: entry.voteAccount,
+          meta: {funder: 'ValidatorBond' as 'ValidatorBond'},
+          reason: 'BidTooLowPenalty' as 'BidTooLowPenalty',
+        }
+        protectedEventsWithValidator.push({
+          status: ProtectedEventStatus.ESTIMATE,
+          protectedEvent,
+          validator,
+        })
+      }
     }
 
     return protectedEventsWithValidator
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,5 +1,6 @@
 import path from "path";
-import webpack, { Configuration } from "webpack";
+import webpack from "webpack";
+import type { Configuration } from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
 import { TsconfigPathsPlugin } from "tsconfig-paths-webpack-plugin";
