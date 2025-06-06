a5975840e37d6c74d0d46be6ce8ae280b60abd63
Ondrej Vostal <ondrej@vostal.net>
2025-06-06 18:10:40 +0200

    Add banner

--- src/services/banner.ts
+++ src/services/banner.ts
@@ -1,7 +1,7 @@
 import { Props } from "src/components/banner/banner";
 
 export const getBannerData = () => ({
-  title: 'Max Stake Wanted will be activated',
-  text: 'Please, all validators set up your bonds properly.',
+  title: "We're bringing Max Stake Wanted back",
+  text: 'Please, review the setup of your bonds so that you do not stop receiving stake once the feature is activated on Wed 11. You can see the current setup in the "Validator Bonds" tab.',
   url: 'http://www.example.com',
 })
