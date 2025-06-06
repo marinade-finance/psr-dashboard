2e51f0815bcb0ba4614af45b72e9ac818c7b8587
Ondrej Vostal <ondrej@vostal.net>
2025-06-06 18:04:39 +0200

    Add banner

---
+++ src/services/banner.ts
@@ -0,0 +1,7 @@
+import { Props } from "src/components/banner/banner";
+
+export const getBannerData = () => ({
+  title: 'Max Stake Wanted will be activated',
+  text: 'Please, all validators set up your bonds properly.',
+  url: 'http://www.example.com',
+})
