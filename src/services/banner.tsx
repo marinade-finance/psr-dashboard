5e275c9727e47f77b58c94c8319c481e114f1e53
Ondrej Vostal <ondrej@vostal.net>
2025-06-09 10:59:01 +0200

    Tune bond and its content

--- src/services/banner.tsx
+++ src/services/banner.tsx
@@ -1,33 +1,37 @@
 import React from "react";
 import { Props } from "src/components/banner/banner";
 
-export const getBannerData = () => ({
-  title: "🚨 MaxStakeWanted is Back – Be Sure to Set It Up 🚨",
+export const getBannerData = (): Props => ({
+  title: "🚨 Max Stake Wanted is Back – Be Sure to Set It Up 🚨",
   body: <>
 
-We are re-enabling MaxStakeWanted, a setting that lets validators cap how much
+<p>
+We are re-enabling max-stake-wanted, a setting that lets validators cap how much
 stake they want to receive from Marinade. This gives validators more control and
 helps them better manage their bonding needs.  Once most validators have set
-their MaxStakeWanted, we plan to relax bonding constraints, allowing more stake
+their max-stake-wanted, we plan to relax bonding constraints, allowing more stake
 with smaller bonds, even at higher bids.
+</p>
 
-<br/>
-<br/>
-💡 You can review your current setup in the "<a href="/bonds">Validator Bonds</a>" tab.
+<p>
+💡 You can review your current setup in the " <a href="/bonds">Validator Bonds</a> " tab.
+</p>
 
-<br/>
-<br/>
+<p>
 A reminder will go out mid-week to make sure everyone is ready.
+</p>
 
-<br/>
-<br/>
-👉 Please set your MaxStakeWanted value as soon as possible to benefit from these changes.
+<p>
+👉 Please set your <strong><u>--max-stake-wanted</u></strong> value as soon as possible to benefit from these changes.
+</p>
 
-<br/>
+<p>
 Be sure to use the newest version of the CLI, at least 2.1.8.
+</p>
 
-<br/>
-<br/>
+<p>
 read more: <a href="https://www.npmjs.com/package/@marinade.finance/validator-bonds-cli?activeTab=readme#bond-account-configuration">Validator Bonds CLI - docs</a>
+</p>
+
 </>,
 })
