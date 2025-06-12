15a1d9d60372b9d517a15a73651a0d1b051605ac
Ondrej Vostal <ondrej@vostal.net>
2025-06-12 17:18:20 +0200

    Update banner

--- src/services/banner.tsx
+++ src/services/banner.tsx
@@ -2,16 +2,18 @@ import React from "react";
 import { Props } from "src/components/banner/banner";
 
 export const getBannerData = (): Props => ({
-  title: "🚨 Max Stake Wanted is Back – Be Sure to Set It Up 🚨",
+  title: "🚨 Bond requirements are now relaxed 🚨",
   body: <>
 
-<p>
-💡 You can review your current setup in the " <a href="/bonds">Validator Bonds</a> " tab.
-</p>
+<p>You can now bid high with a reasonably sized bond.</p>
 
 <p>
 👉 Please set your <strong><u>--max-stake-wanted</u></strong> to benefit from these changes.
 <br/> Note that you can never request less than 10k SOL from the auction.
+</p>
+
+<p>
+💡 You can review your current setup in the " <a href="/bonds">Validator Bonds</a> " tab.
 </p>
 
 <p>
