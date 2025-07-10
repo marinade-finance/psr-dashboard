a0308d083396a384309064b3c94794312a4f5cec
kronael <ondrej@vostal.net>
2025-07-10 14:07:14 +0200

    [GEN-5636] Add stake matching banner (#42)
    * Add stake matching banner
    
    * Open in a blank page

--- src/services/banner.tsx
+++ src/services/banner.tsx
@@ -2,6 +2,20 @@ import React from "react";
 import { Props } from "src/components/banner/banner";
 
 export const getBannerData = (): Props => ({
-  title: "",
-  body: <></>,
+  title: "🚨 Stake Matching Now Live 🚨",
+  body: <>
+<p>
+Marinade now matches 10% to 30% of external stake you attract, with no bond required for the matched portion.
+</p>
+
+<p>
+You must still win stake through the auction to receive matching.
+</p>
+
+<p>
+Up to 0.4% of Marinade TVL can be matched per validator.
+</p>
+
+Learn more in the <a href="https://docs.marinade.finance/marinade-protocol/protocol-overview/stake-auction-market#stake-matching" target="_blank">docs</a>.
+</>,
 })
