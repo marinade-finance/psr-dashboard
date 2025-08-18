d07335147a39070f030d3a485c112257a84fbb51
kronael <ondrej@vostal.net>
2025-08-18 12:55:16 +0200

    Fixed Max APY display [GEN-5188] (#46)
    * Fixed Max APY display
    
    * Improve

--- src/format.ts
+++ src/format.ts
@@ -3,13 +3,21 @@ import round from 'lodash.round'
 export const formatSolAmount = (amount: number, digits = 2) =>
   round(amount, digits).toLocaleString()
 
-export const formatPercentage = (amount: number, fractionDigits = 2) => {
-  if (amount >= Infinity) {
-    return '∞'
-  } else if (amount <= -Infinity) {
-    return '-∞'
+export const formatPercentageString = (amount: number, fractionDigits: number = 2): string => {
+  const x = round(100 * amount, fractionDigits)
+  const str = (fractionDigits > 0) ? x.toFixed(fractionDigits) : x
+  return `${str}%`
+}
+
+export const formatPercentage = (amount: number, fractionDigits: number = 2, maxValue: number = 1e18): string => {
+  if (amount >= maxValue) {
+    const maxValueLabel = formatPercentageString(maxValue, fractionDigits)
+    return `>${maxValueLabel}`
+  } else if (amount <= -maxValue) {
+    const maxValueLabel = formatPercentageString(maxValue, fractionDigits)
+    return `<-${maxValueLabel}`
   }
-  return `${round(100 * amount, fractionDigits)}%`
+  return formatPercentageString(amount, fractionDigits)
 }
 
 export const lamportsToSol = (lamports: string) =>
