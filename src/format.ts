74fe3792baff5f1e2e7bfc60aaeafeef7d9f1365
Ondrej Vostal <ondrej@vostal.net>
2025-08-18 12:45:05 +0200

    Improve

--- src/format.ts
+++ src/format.ts
@@ -3,17 +3,21 @@ import round from 'lodash.round'
 export const formatSolAmount = (amount: number, digits = 2) =>
   round(amount, digits).toLocaleString()
 
-export const formatPercentage = (amount: number, fractionDigits: number = 2, maxValue: number = 1e18): string => {
-  if (amount >= maxValue) {
-    const maxValueLabel = formatPercentage(maxValue, fractionDigits, maxValue + 1)
-    return `>${maxValueLabel}`
-  } else if (amount <= -maxValue) {
-    const maxValueLabel = formatPercentage(maxValue, fractionDigits, maxValue + 1)
-    return `<-${maxValueLabel}`
-  }
+export const formatPercentageString = (amount: number, fractionDigits: number = 2): string => {
   const x = round(100 * amount, fractionDigits)
   const str = (fractionDigits > 0) ? x.toFixed(fractionDigits) : x
   return `${str}%`
+}
+
+export const formatPercentage = (amount: number, fractionDigits: number = 2, maxValue: number = 1e18): string => {
+  if (amount >= maxValue) {
+    const maxValueLabel = formatPercentageString(maxValue, fractionDigits)
+    return `>${maxValueLabel}`
+  } else if (amount <= -maxValue) {
+    const maxValueLabel = formatPercentageString(maxValue, fractionDigits)
+    return `<-${maxValueLabel}`
+  }
+  return formatPercentageString(amount, fractionDigits)
 }
 
 export const lamportsToSol = (lamports: string) =>
