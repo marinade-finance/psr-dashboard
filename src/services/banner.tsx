db7bfe4af494f34785d17079dc19fd225f36a9d1
Ondra Chaloupka <chalda@marinade.finance>
2025-12-17 11:50:56 +0100

    [banner] dynamic commission announcement

--- src/services/banner.tsx
+++ src/services/banner.tsx
@@ -2,18 +2,14 @@ import React from "react";
 import { Props } from "src/components/banner/banner";
 
 export const getBannerData = (): Props => ({
-  title: "",
+  title: "✓✓✓ Introducing Dynamic Commissions ✓✓✓",
   body: <>
 <p>
-📜 Marinade banned validators intentionally degrading Solana's performance.
-</p>
-
-<p>
-Now their stake is getting re-distributed towards honest validators.
-</p>
-
-<p>
-🧠 Make sure to top-up your bond to <strong>receive part of the re-distributed stake</strong>.
+📜 Validators can bid not only with a static bid (<code>--cpmpe</code>),
+but also by setting commissions on inflation, MEV, and block rewards in bond,
+which are used to calculate the effective auction bid.
+See more at <a target="_blank" href="https://docs.marinade.finance/marinade-protocol/protocol-overview/stake-auction-market">Marinade Doc page</a>
+&nbsp;and <a target="_blank" href="https://github.com/marinade-finance/validator-bonds/tree/main/packages/validator-bonds-cli#core-concepts">Validator Bonds CLI README</a>
 </p>
 </>,
 })
