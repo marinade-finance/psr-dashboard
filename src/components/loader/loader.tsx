033646c63af3dc9695de76ca3ea86e65767173a4
Ondrej Vostal <ondrej@vostal.net>
2026-02-26 17:23:17 +0100

    [style] Center loader spinner in viewport

--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Loader: React.FC = () => {
   return (
-    <div className="flex items-center justify-center py-32">
+    <div className="flex items-center justify-center min-h-[60vh]">
       <div className="w-10 h-10 border-4 border-primary/30 border-t-primary rounded-full animate-spin" />
     </div>
   )
