e735de7acfd589e435d3e4278ec2b588c665c8f7
Ondrej Vostal <ondrej@vostal.net>
2026-02-26 17:13:51 +0100

    [style] Replace text loader with centered spinning circle

--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
 export const Loader: React.FC = () => {
-  return <div className="m-2.5 loader-shift">Loading </div>
+  return (
+    <div className="flex items-center justify-center py-32">
+      <div className="w-10 h-10 border-4 border-primary/30 border-t-primary rounded-full animate-spin" />
+    </div>
+  )
 }
