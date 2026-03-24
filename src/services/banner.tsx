adfb4020ac405007114365b7ccbba9847fecc9a3
kron <ondrej@vostal.net>
2026-03-24 15:45:29 +0100

    Apply suggestion from @kronael

--- src/services/banner.tsx
+++ src/services/banner.tsx
@@ -7,7 +7,7 @@ export const getBannerData = (): Props => ({
   body: (
     <>
       <p>
-        Per-validator stake cap raised from 8% to 15% of TVL, completing MIP-19.
+        Per-validator stake cap raised from 8% to 15% of TVL, moving yet a bit closer to MIP-19 completion.
         Following the successful Step 1 raise at Epoch 924, top-performing
         validators can now acquire even more stake by adjusting{' '}
         <code>maxStakeWanted</code>, topping up bonds and offering competitive
