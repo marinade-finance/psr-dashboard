5072bc4a1f27a6ac61686ba6398be8dcc01cbc85
chalda <chalda@marinade.finance>
2026-04-07 19:01:57 +0200

    [sdk sam] update version to 0.0.45 / auction override fixes / additional overrides removal (#115)

--- src/services/banner.tsx
+++ src/services/banner.tsx
@@ -7,11 +7,11 @@ export const getBannerData = (): Props => ({
   body: (
     <>
       <p>
-        Per-validator stake cap raised from 8% to 15% of TVL, moving yet a bit closer to MIP-19 completion.
-        Following the successful Step 1 raise at Epoch 924, top-performing
-        validators can now acquire even more stake by adjusting{' '}
-        <code>maxStakeWanted</code>, topping up bonds and offering competitive
-        bids.
+        Per-validator stake cap raised from 8% to 15% of TVL, moving yet a bit
+        closer to MIP-19 completion. Following the successful Step 1 raise at
+        Epoch 924, top-performing validators can now acquire even more stake by
+        adjusting <code>maxStakeWanted</code>, topping up bonds and offering
+        competitive bids.
       </p>
     </>
   ),
