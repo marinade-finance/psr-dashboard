3561eab7913bd33d218ae16e3cd4a09c070b3635
kron <ondrej@vostal.net>
2026-04-21 23:55:14 +0200

    [banner] Update for BRRM launch [GEN-7372] (#123)
    * [banner] Update for BRRM launch
    
    * Apply suggestion from @kronael

--- src/services/banner.tsx
+++ src/services/banner.tsx
@@ -3,17 +3,30 @@ import React from 'react'
 import type { Props } from 'src/components/banner/banner'
 
 export const getBannerData = (): Props => ({
-  title: 'Bond Notifications Are Now Live',
+  title: '📢 📢 📢 Bond Risk Reduction Mechanism now active',
   body: (
     <>
       <p>
-        Stay on top of your bond without manual checks. Subscribe to get alerts
-        for underfunding, auction exits, eligibility changes, and more — via
-        Telegram or email.
+        Validators with insufficient bond coverage (with "Cover. [ep]" at or
+        less than zero) now pay a fee to cover the redelegation of their
+        stake.&nbsp;
+        <a
+          href="https://docs.marinade.finance/marinade-protocol/protocol-overview/stake-auction-market/bond-risk-reduction-mechanism"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Find out details↗
+        </a>
       </p>
       <p>
-        <a href="https://docs.marinade.finance/marinade-protocol/protocol-overview/stake-auction-market/bond-notifications">
-          Learn more
+        Stay on top of your bond coverage. Subscribe to get alerts for
+        underfunding via Telegram or email.&nbsp;
+        <a
+          href="https://docs.marinade.finance/marinade-protocol/protocol-overview/stake-auction-market/bond-notifications"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn more↗
         </a>
       </p>
     </>
