50c5ec13e1d8712fc7adf162ec33feb8b7a4ba42
Ondrej Vostal <ondrej@vostal.net>
2026-05-11 20:21:37 +0000

    [visual] nav: logo 34→41px (+20%)

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -18,8 +18,8 @@ export type UserLevelProps = {
 
 const MarinadeLogo = () => (
   <svg
-    width="34"
-    height="34"
+    width="41"
+    height="41"
     viewBox="0 0 50 33"
     fill="none"
     className="text-primary shrink-0"
