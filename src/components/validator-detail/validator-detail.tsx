d74c157ed1ea2be014ad44868254a1f19c9a5630
Ondrej Vostal <ondrej@vostal.net>
2026-05-15 10:09:18 +0000

    [fix] validator-detail: tab strip sticky offset matches header height

--- src/components/validator-detail/validator-detail.tsx
+++ src/components/validator-detail/validator-detail.tsx
@@ -505,7 +505,7 @@ export const ValidatorDetail = ({
 
           return (
             <>
-              <div className="border-b border-border bg-background sticky top-[96px] z-[5]">
+              <div className="border-b border-border bg-background sticky top-[68px] z-[5]">
                 <div className="flex gap-1 px-4 sm:px-6 overflow-x-auto">
                   {TAB_DEFS.map(t => (
                     <button
